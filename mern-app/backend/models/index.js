d6330535e9df4ce5e8aa747d305ae30655a09de4
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-21 10:14:26 -0700

    Backend added

---
+++ mern-app/backend/models/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const mongodbUri = process.env.MONGODBURI;
+
+// ASYNC FUNCTION 
+// CONNECTION TO MONGODB ATLAS
+(async function () {
+    await mongoose.connect(mongodbUri)
+    console.log('Mongoose is connected to ', mongodbUri)
+})().catch(err => console.log('Monngoose connection error:\n' + err))
+
+// Exporting models and seed data to server.js
---
+++ mern-app/backend/server.js
@@ -0,0 +1,30 @@
+/************ REQUIRING MODELS*/
+require('dotenv').config()
+const express = require('express')
+
+
+/************** REQUIRING DB CONNECTION, MODELS, SEED DATA */
+const db = require('./models')
+
+/************** REQUIRING ROOTES FROM CONTROLLERS */
+
+
+
+
+/************** CREATING EXPRESS APP */
+const app = express();
+
+
+/*************** MIDDLEWARE  */
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+/*************** MOUTING ROUTES */
+
+
+
+/*************** PORT */
+app.listen(process.env.PORT, function () {
+    console.log('Express is listening to port', process.env.PORT)
+})
