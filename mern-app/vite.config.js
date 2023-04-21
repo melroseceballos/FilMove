2aa544d292d3899ad9a44f44df7be7d13238040e
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-21 10:18:10 -0700

    configured package.json and vite.config

--- mern-app/vite.config.js
+++ mern-app/vite.config.js
@@ -1,7 +1,22 @@
+import 'dotenv/config';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    root: path.resolve(__dirname, 'frontend'),
+    envFile: path.resolve(__dirname, '.env'),
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            }
+        }
+    }
 })
+
