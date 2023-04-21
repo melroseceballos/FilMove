b4ae41dc6374c23083737228b6e0d46bf4754224
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-21 12:47:02 -0700

    tried to give pagination a shot , did not work -- will move on and revisit later

--- mern-app/frontend/src/components/HomePage/home.jsx
+++ mern-app/frontend/src/components/HomePage/home.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 
 function Home() {
     const [movies, setMovies] = useState ([])
+    const [page, setPage] = useState (1)
 
         // using async function to fetch my data
     useEffect(()=>{
@@ -12,9 +13,27 @@ function Home() {
             setMovies(results)
             console.log(movies)
         }
-        // CALLING FUNCTIONS HERE
+        // calling getData() function here
         getData()
     }, []);
+
+            /// WILL FIGURE OUT PAGINATION LATER IF THERE IS TIME
+    //     // using useEffect and creating another function for pagination
+    // useEffect(() =>{
+    //     async function fetchMovies(){
+    //         const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=50760a302380745ede7e1c3eee6ca282&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`)
+    //         const {results} = await res.json()
+    //         // creates a new array called prevMovies from the array ...pages
+    //         setPage(prevMovies)
+    //     }
+    //     // calling fetchMovies() functions here
+    //     fetchMovies()
+    // }, [page]);
+
+    // // creating an onclick function that increments page by 1 using the setPage
+    // function nextPage(){
+    //     setPage((prevPage) => prevPage + 1 )
+    // }
     return(
         <>
         <div className='movieDiv'>
@@ -25,10 +44,16 @@ function Home() {
                     <h1 className='movieTitle'>{movie.title}</h1>
                     <img key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
                     </div>
+                    
                     </>
                 )
             ))}
-
+           
+            
+        </div>
+        <div className='buttonContainers'>
+            <button>Previous Page</button>
+            <button>NextPage</button>
         </div>
         </>
     )
