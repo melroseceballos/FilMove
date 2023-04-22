f178f29f921c04266670509e325a88722cee9b1f
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-21 21:29:45 -0700

    Create GenrePage

--- mern-app/frontend/src/components/App/App.jsx
+++ mern-app/frontend/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import './styles.css'
 import Home from '../HomePage/home'
 import { Routes, Route, Link } from 'react-router-dom'
 import Movie from '../MoviePage/movie'
+import Genre from '../GenrePage/genre'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
     <nav>
       <Link to ='/'>Home</Link>
-      <Link to='#'> Genre </Link>
+      <Link to='/genre'>Genre</Link>
       <Link to='#'>Top 100</Link>
       <h1>FilMove</h1>
       <Link to='#'>Watch Now</Link>
@@ -20,6 +21,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movie/:id' element={<Movie />} />
+        <Route path='/genre' element={<Genre />} />
+       
       </Routes>
     </main>
     </>
---
+++ mern-app/frontend/src/components/GenrePage/genre.jsx
@@ -0,0 +1,39 @@
+import './styles.css'
+
+function Genre()    {
+    return(
+        <>
+        <br />
+        <br />
+        <br />
+        <h1 className='genreTitle'>Choose Your Genre</h1>
+       <div className='genreDiv'>
+       <div className='card'>Indigenous </div>
+       <div className='card'>Horror</div>
+        <div className='card'>Action</div>
+        <div className='card'>Comedy</div>
+        <div className='card'>Romance</div>
+        <div className='card'>Drama</div>
+        <div className='card'>Science Fiction</div>
+        <div className='card'>LGBTQ</div>
+        <div className='card'>Thriller</div>
+        <div className='card'>Documentary</div>
+        <div className='card'>Western</div>
+        <div className='card'>Animation</div>
+        <div className='card'>Anime</div>
+        <div className='card'>Musical</div>
+        <div className='card'>True Crime</div>
+        <div className='card'>War Movies</div>
+        <div className='card'>Mystery</div>
+        <div className='card'>Independent</div>
+        <div className='card'>Foreign</div>
+        <div className='card'>Adventure</div>
+        <div className='card'>Shorts</div>
+
+       </div>
+        </>
+        
+    )
+}
+
+export default Genre
