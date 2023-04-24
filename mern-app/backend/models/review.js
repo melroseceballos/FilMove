408687fefa79309b5c3ebf63b291471d632bda23
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-24 14:53:36 -0700

    added user to review model

--- mern-app/backend/models/review.js
+++ mern-app/backend/models/review.js
@@ -5,6 +5,11 @@ const reviewSchema = new mongoose.Schema({
     rate: {type: Number, required: true},
     content: {type: String, required: true},
     movieId: {type: String, required: true},
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: user,
+    }
 }
     
 )
