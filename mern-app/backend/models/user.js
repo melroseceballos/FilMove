738c7853167a9784d0f92d364cb1940d9c032697
Melrose Ceballos <mcm44@hawaii.edu>
2023-04-24 14:51:22 -0700

    created user.js model

---
+++ mern-app/backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+	email: { 
+        type: String,
+        unique: true,
+        required: true
+    },
+	password: { 
+        type: String, 
+        minlength: 6,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
